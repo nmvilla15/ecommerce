8e02375058ba855e1f01f8bda7736ea9234af13e
nmvilla15 <niki.villalpando@gmail.com>
2021-03-04 22:02:13 -0600

    moved config folder

--- src/main/java/com/tts/ecommerce/configuration/SecurityConfiguration.java
+++ src/main/java/com/tts/ecommerce/configuration/SecurityConfiguration.java
@@ -1,4 +1,4 @@
-package configuration;
+package com.tts.ecommerce.configuration;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
--- src/main/java/com/tts/ecommerce/configuration/WebMvcConfiguration.java
+++ src/main/java/com/tts/ecommerce/configuration/WebMvcConfiguration.java
@@ -1,4 +1,4 @@
-package configuration;
+package com.tts.ecommerce.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
